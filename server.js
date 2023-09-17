a3082f9849f0cb8cec367322f58087f3d9f7b9c5
SycoSonic <christopher.jones29@gmail.com>
2023-09-17 14:57:24 +1000

    Initial commit with basic backend setup and LangChain integration.

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { OpenAI } = require("langchain/llms/openai");
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
